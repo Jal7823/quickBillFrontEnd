4373e5d091ca7e847e796494716f80dc6751966d
jal7823 <jal7823@gmail.com>
2023-05-13 12:14:08 -0300

    feat: :zap: Start project
    Start project

---
+++ quickBillFrontEnd/src/App.jsx
@@ -0,0 +1,20 @@
+import Asside from './pages/Asside'
+import Content from './pages/Content'
+import Footer from './pages/Footer'
+import Nabvar from './pages/Nabvar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Nabvar />
+      <Asside />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+export default App
